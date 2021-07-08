4b5f80609d4f1f69e53946a59325b06106cf3e5b
Ha Long <longvn00@gmail.com>
2021-07-08 18:12:10 +0700

    use version placeholder

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import express from "express";
 
 const app = express();
+const VERSION = '0';
 
 app.get('/', (req, res) => {
-    res.send('Hello world from example-service: version 1\n');
+    res.send(`Hello world from example-service: version ${VERSION}\n`);
 })
 const PORT = 80;
 app.listen(PORT, () => {
-    console.log('sample v1');
+    console.log(`sample version: ${VERSION}`);
     console.log('Server running at localhost:' + PORT)
 })
