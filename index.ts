c5cf7cd3ec16a24031aa9afa24a42781d0387b42
Ha Long <longvn00@gmail.com>
2021-07-12 17:32:25 +0700

    update change

--- index.ts
+++ index.ts
@@ -4,10 +4,10 @@ const app = express();
 const VERSION = '0';
 
 app.get('/', (req, res) => {
-    res.send(`Hello world from example-service: version ${VERSION}\n`);
+    res.send(`Hello world from sample-service: version ${VERSION}\n`);
 })
 const PORT = 80;
 app.listen(PORT, () => {
-    console.log(`sample version: ${VERSION}`);
-    console.log('Server running at localhost:' + PORT)
+    console.log(`Sample version: ${VERSION}`);
+    console.log('Server running at localhost:' + PORT);
 })
