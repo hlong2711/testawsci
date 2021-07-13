37ea972828f7390f584bfd49c9e921010781d091
Ha Long <longvn00@gmail.com>
2021-07-13 10:14:27 +0700

    add variable

--- index.ts
+++ index.ts
@@ -3,11 +3,13 @@ import express from "express";
 const app = express();
 const VERSION = '0';
 
+const TEST_ENV_VAR = process.env.TEST_ENV_VAR;
+
 app.get('/', (req, res) => {
     res.send(`Hello world from sample-service: version ${VERSION}\n`);
 })
 const PORT = 80;
 app.listen(PORT, () => {
-    console.log(`Sample version: ${VERSION}`);
+    console.log(`Sample version: ${VERSION}, var: ${TEST_ENV_VAR}`);
     console.log('Server running at localhost:' + PORT);
 })
